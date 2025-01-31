ea83515c7940f8fc56e6fe1e687ab47687cd3039
Mi-aro-101 <miaroramanandraitsiory@gmail.com>
2025-01-31 11:54:17 +0300

    Staging first version of ApiLibrary for test dev use

---
+++ Api/ApiResponse.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+
+namespace LimsUtils.Api;
+
+public class ApiResponse
+{
+
+    public void HandleResponse<T>()
+    {
+        if(this?.Data != null)
+        {
+            var jsonString = JsonSerializer.Serialize(this.Data);
+            var departementDtos = JsonSerializer.Deserialize<T>(jsonString);
+            if(departementDtos != null) this.Data = departementDtos;
+        }
+    }
+
+    public object? Data { get; set; }
+    public Dictionary<string, object>? ViewBag { get; set; }
+    public string? Message { get; set; }
+    public bool IsSuccess { get; set; }
+    public int StatusCode { get; set; }
+}
